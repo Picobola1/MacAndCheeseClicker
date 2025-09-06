de2ec0d0d92c2af664e2f9a0b345ae520356e5b8
Ellie Bonilla <bonilla.ellie@gmail.com>
2025-09-06 09:05:17 -0700

    onClickLooksBetter

--- index.js
+++ index.js
@@ -3,13 +3,21 @@ MacCheese = 4;
 let MacOnClick = 1;
 const PointsLabel =  document.getElementById("MacCheesePoints")
 const Mac1 = document.getElementById("Mac1")
+const Mac1Img = document.getElementById("Mac1Img")
 
 
 function UpdatePoints() {
    PointsLabel.textContent = "Points: " + MacCheese;
 }
 function MacClick(){
-    MacCheese += MacOnClick
+    MacCheese += MacOnClick;
+    
+    
 }
+
+
+
 Mac1.addEventListener("click", MacClick);
+
+
 setInterval(UpdatePoints, 100);
