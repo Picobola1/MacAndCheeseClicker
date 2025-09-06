8f96f9a984013f20a2018b4d333ba82450d91795
Ellie Bonilla <bonilla.ellie@gmail.com>
2025-09-06 08:36:07 -0700

    Macking it look sligthy prettier

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let MacCheese;
 MacCheese = 4;
 const PointsLabel =  document.getElementById("MacCheesePoints")
 
+
 function UpdatePoints() {
    PointsLabel.textContent = "Points: " + MacCheese;
 }
