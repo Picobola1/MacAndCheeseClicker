c2d08cf13ee1d8579ac345592d927925b8f2d8ce
Ellie Bonilla <bonilla.ellie@gmail.com>
2025-09-06 08:51:14 -0700

    ButtonWorking

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 let MacCheese;
 MacCheese = 4;
+let MacOnClick = 1;
 const PointsLabel =  document.getElementById("MacCheesePoints")
+const Mac1 = document.getElementById("Mac1")
 
 
 function UpdatePoints() {
    PointsLabel.textContent = "Points: " + MacCheese;
 }
-
+function MacClick(){
+    MacCheese += MacOnClick
+}
+Mac1.addEventListener("click", MacClick);
 setInterval(UpdatePoints, 100);
