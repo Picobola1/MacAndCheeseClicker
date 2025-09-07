444938d594013e7c09607ef5c1f3b862cd3b3d96
Ellie Bonilla <bonilla.ellie@gmail.com>
2025-09-06 21:55:29 -0700

    WorkingButton

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
 let MacCheese;
 MacCheese = 4;
 let MacOnClick = 1;
+let Mac1Bought = 0;
 const PointsLabel =  document.getElementById("MacCheesePoints")
 const Mac1 = document.getElementById("Mac1")
 const Mac1Img = document.getElementById("Mac1Img")
+const ButtonMac1 = document.getElementById("ButtonMac1")
 
 
-function UpdatePoints() {
-   PointsLabel.textContent = "Points: " + MacCheese;
-}
+
 function MacClick(){
     MacCheese += MacOnClick;
     
     
 }
+function fButtonMac1(){
+    if (MacCheese >= 5){
+        MacOnClick += 1 
+        MacCheese -= 5
+        Mac1Bought++ 
+        ButtonMac1.style.backgroundColor = 'orange' 
+    } else {
+        ButtonMac1.style.backgroundColor = 'black'
+    }
+    
+    
+}
+function UpdateText(){
+    ButtonMac1.textContent = "+1 Per Click🧀🧀🧀🧀: " + Mac1Bought
+    PointsLabel.textContent = "Points: " + MacCheese;
+}
+
 
 
 
 Mac1.addEventListener("click", MacClick);
+ButtonMac1.addEventListener("click", fButtonMac1)
 
 
-setInterval(UpdatePoints, 100);
+setInterval(UpdateText, 100);
